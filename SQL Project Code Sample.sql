543f422337b1c55715e7d25e5f22f0b214dd77a5
Otishq <134025482+Otishq@users.noreply.github.com>
2023-05-19 09:30:09 +0700

    SQL Project Code Sample.sql

---
+++ SQL Project Code Sample.sql
@@ -0,0 +1,74 @@
+
+-- Show sale data table & order by account_id, total_amt_usd
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY account_id, total_amt_usd DESC;
+
+--Join region, accounts to get name
+
+SELECT r.name region, s.name rep, a.name account
+FROM sales_reps s
+JOIN region r
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+WHERE r.name = 'Midwest' AND s.name LIKE '% K%'
+ORDER BY a.name;
+
+-- SQL Subqueries
+
+SELECT COUNT(*)
+FROM (SELECT a.name
+          FROM orders o
+          JOIN accounts a
+          ON a.id = o.account_id
+          GROUP BY 1
+          HAVING SUM(o.total) > (SELECT total 
+                      FROM (SELECT a.name act_name, SUM(o.standard_qty) tot_std, SUM(o.total) total
+                            FROM accounts a
+                            JOIN orders o
+                            ON o.account_id = a.id
+                            GROUP BY 1
+                            ORDER BY 2 DESC
+                            LIMIT 1) inner_tab)
+                ) counter_tab
+-- Temporary Tables
+
+WITH t1 AS (
+     SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+      FROM sales_reps s
+      JOIN accounts a
+      ON a.sales_rep_id = s.id
+      JOIN orders o
+      ON o.account_id = a.id
+      JOIN region r
+      ON r.id = s.region_id
+      GROUP BY 1,2
+      ORDER BY 3 DESC), 
+t2 AS (
+      SELECT region_name, MAX(total_amt) total_amt
+      FROM t1
+      GROUP BY 1)
+SELECT t1.rep_name, t1.region_name, t1.total_amt
+FROM t1
+JOIN t2
+ON t1.region_name = t2.region_name AND t1.total_amt = t2.total_amt;
+
+--Union with accounts
+
+WITH double_accounts AS (
+    SELECT *
+      FROM accounts
+    
+    UNION ALL
+    
+    SELECT *
+      FROM accounts
+)
+
+SELECT name,
+       COUNT(*) AS name_count
+ FROM double_accounts 
+GROUP BY 1
+ORDER BY 2 DESC
